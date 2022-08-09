5658710cbe6ef3f1a8fc70e651640b4e1f82130d
abelardom3 <abelardom1999@gmail.com>
2022-08-09 11:18:13 -0400

    add changes

--- app.js
+++ app.js
@@ -2,16 +2,213 @@
 // I don't want just lines and lines of code written in the global scope or in one huge function.
 
 // When page loads, make a get request that gets all users and creates divs for each user. 
-    // Each user div should have the users name, username, and city they are located in.
-    // when a div is clicked on, it should fetch all posts associated with the user id.
+// Each user div should have the users name, username, and city they are located in.
+// when a div is clicked on, it should fetch all posts associated with the user id.
+
+// const container = document.querySelector('#container')
+
+// const containerPost = document.createElement("div")
+// containerPost.id = "containerPost"
+
+// // example()
+
+// // function example() {
+// //     $.get([some url here], [some callback function that gets access to data here])
+// // }
+
+// getUsers()
+
+
+
+
+// function getUsers() {
+//     $.get("https://jsonplaceholder.typicode.com/users", (data) => {
+//         console.log(data)
+//     })
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const container = document.querySelector('#container')
+
+
+const containerPost = document.createElement("div")
+containerPost.id = "containerPost"
+
+const backButton = document.createElement('button')
+backButton.className = 'backBtn'
 
 // example()
 
 // function example() {
 //     $.get([some url here], [some callback function that gets access to data here])
 // }
+
+buttonEvent()
+
+
+function buttonEvent() {
+    const getBtn = document.getElementById("btn")
+    getBtn.addEventListener('click', getUsers)
+
+
+}
+
+function getUsers() {
+    $.get("https://jsonplaceholder.typicode.com/users", logUsers)
+
+}
+
+
+function logUsers(userInfo) {
+    for (var i = 0; i < userInfo.length; i++) {
+        const currentUser = userInfo[i]
+        createDiv(currentUser)
+
+    }
+
+
+}
+
+function createDiv(object) {
+    container.classList = "users"
+    const currentUserDiv = document.createElement('div')
+    currentUserDiv.className = "homePage"
+    currentUserDiv.id = object.id
+    currentUserDiv.innerText = `Name: ${object.name}, Username: ${object.username}, City: ${object.address.city}`
+    container.appendChild(currentUserDiv)
+    clickId(object.id)
+
+}
+
+function clickId(id) {
+    const idUser = document.getElementById(id)
+    idUser.addEventListener('click', function (e) {
+        const id = e.target.id
+        $.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`, getPost)
+
+        hideContainer()
+        hideGetBtn()
+    })
+
+}
+
+
+function getPost(postData) {
+    for (var i = 0; i < postData.length; i++) {
+        var userPost = postData[i]
+        createDivPost(userPost)
+    }
+
+}
+
+function createDivPost(userPost) {
+    const postDiv = document.createElement("div")
+    postDiv.className = "postPage"
+    postDiv.innerText = `Posted: "${userPost.title}"`
+    containerPost.append(postDiv)
+    document.body.append(containerPost)
+    showContainer()
+}
+
+function showContainer() {
+    $(containerPost).before(backButton)
+    backButton.innerText = "Go back"
+    backButton.addEventListener('click', () => {
+        $(container).show()
+        hidePost()
+
+    })
+
+}
+
+function hidePost() {
+    $(containerPost).hide()
+    $(containerPost).empty()
+    $(backButton).hide()
+    showGetBtn()
+}
+
+
+
+function hideContainer() {
+    $(container).hide()
+    $(containerPost).show()
+    $(backButton).show()
+
+}
+
+function hideGetBtn() {
+    const getBtn = document.getElementById('btn')
+    console.log(getBtn)
+    $(getBtn).hide()
+}
+
+function showGetBtn() {
+    const getBtn = document.getElementById('btn')
+    $(getBtn).show()
+}
+
+
+
+
+
+
+
+
+
 
 
 
